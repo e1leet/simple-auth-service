164473420f5b9e5cee1f76f96d3bdb8cfe978ccb
Damir Mirasov <damirmirasovmain@gmail.com>
2022-12-29 13:27:00 +0600

    Add config tests

---
+++ internal/config/config_test.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testDataFolder = "testdata/"
+
+func TestLoadConfig(t *testing.T) {
+	t.Run("non_existent_file", func(t *testing.T) {
+		_, err := LoadConfig("dfklajflkjd")
+		assert.ErrorIs(t, err, os.ErrNotExist)
+	})
+
+	t.Run("load_config_yaml", func(t *testing.T) {
+		expected := &Config{Server: ServerConfig{
+			Host:            "127.0.0.1",
+			Port:            5000,
+			ShutdownTimeout: 10 * time.Second,
+			IsProduction:    true,
+			Addr:            "127.0.0.1:5000",
+		}}
+		actual, err := LoadConfig(testDataFolder + "correct.yaml")
+
+		assert.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	})
+}
